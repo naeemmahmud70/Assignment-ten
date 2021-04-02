378d5ff5c161d040f4367c637c9f9d22ab3fc60d
Naeem Mahmud Al-Amin <naeemmahmud370@gmail.com>
2021-04-02 19:24:45 +0600

    first commit

---
+++ client site/README.md
@@ -0,0 +1,9 @@
+Hollow!!!
+Welcome to the FoodLand.
+This is a E-commerce website where you can buy any kind of grocery food. Here you can buy many kinds of products with price and weight. If you click buy button to buy something you have to login. After login you will see your ordered items. Then you have to click checkOut button and than you will see you submitted order with name, price, quantity and ordered date.
+
+Here i used most popular language. For creating frontend i used html, css, bootstrap, javascript, react. And i used nodejs, express and mongodb for backend.
+
+This sites live link----
+This sites github link----
+This sited database link---
---
+++ client site/src/components/Home/Admin/Admin.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
+import DeleteProduct from '../../DeleteProduct/DeleteProduct';
+import './Admin.css'
+
+const Admin = () => {
+    const { register, handleSubmit, watch, errors } = useForm();
+    const [imageURL, setImageURL] = useState(null)
+    console.log(imageURL)
+
+
+    const onSubmit = data => {
+        const productData = {
+            name: data.name,
+            price: data.price,
+            imageURL: imageURL
+        };
+        const url = `https://tranquil-dawn-17124.herokuapp.com/addProduct`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(productData)
+        })
+            .then(res => console.log('server side response', res))
+    };
+
+
+    const handleImageUpload = event => {
+        const imageData = new FormData();
+        imageData.set('key', 'c27cdbd7f672caa5d177ddecda022824');
+        imageData.append('image', event.target.files[0]);
+
+        axios.post('https://api.imgbb.com/1/upload',
+            imageData)
+            .then(function (response) {
+                setImageURL(response.data.data.display_url);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+
+    }
+    return (
+        <div className="row">
+            <div className="col-md-6 shadow p-2   mt-3">
+                <DeleteProduct></DeleteProduct>
+            </div>
+            <div className="col-md-6 shadow p-3 mt-3">
+                <h3 className="text-success">Add product:</h3>
+                <hr/>
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+                    <input name="name" defaultValue="upload new product" ref={register} />
+                    <br />
+                    <input name="price" defaultValue="set product price" ref={register} />
+                    <br />
+                    <input name="exampleRequired" type="file" onChange={handleImageUpload} />
+                    <br />
+
+
+                    <input type="submit" />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Admin;
